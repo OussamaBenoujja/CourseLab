386eda219c0c86ce919751683add2c8f1164e945
OussamaBenoujja <osama2code79@gmail.com>
2025-01-15 12:10:34 +0100

    updated the course class

--- control/Course.php
+++ control/Course.php
@@ -25,12 +25,98 @@ class Course
         $this->content_type = $content_type;
     }
 
-    //getters
-
+    // Getters
     public function getCourseId()
     {
         return $this->course_id;
     }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getTeacherId()
+    {
+        return $this->teacher_id;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function getBannerImage()
+    {
+        return $this->banner_image;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function getContentType()
+    {
+        return $this->content_type;
+    }
+
+    // Setters
+    public function setCourseId($course_id)
+    {
+        $this->course_id = $course_id;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function setTeacherId($teacher_id)
+    {
+        $this->teacher_id = $teacher_id;
+    }
+
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    }
+
+    public function setBannerImage($banner_image)
+    {
+        $this->banner_image = $banner_image;
+    }
+
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    public function setContentType($content_type)
+    {
+        $this->content_type = $content_type;
+    }
+
 
 
 }
