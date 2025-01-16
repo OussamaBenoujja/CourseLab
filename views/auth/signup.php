354688ca8a0b62674b24384c47634fdc1a599a17
OussamaBenoujja <osama2code79@gmail.com>
2025-01-16 20:30:00 +0100

    Implemented authentication views for login and registration.

---
+++ views/auth/signup.php
@@ -0,0 +1,33 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp</title>
+</head>
+<body>
+    
+<main>
+
+    <form>
+
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" required>
+        
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" required>
+        
+        <label for="confirm_password">Confirm Password</label>
+        <input type="password" name="confirm_password" id="confirm_password" required>
+        
+        <button type="submit">SignUp</button>
+
+    </main>
+
+
+</body>
+</html>
