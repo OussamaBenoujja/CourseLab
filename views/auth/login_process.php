64d8a6396292f97a572abf67a1820a005b947fe7
OussamaBenoujja <osama2code79@gmail.com>
2025-01-18 01:18:27 +0100

    updated the login page

--- views/auth/login_process.php
+++ views/auth/login_process.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+
+
 require_once __DIR__ . '/../../control/User.php';
 require_once __DIR__ . '/../../data/db_config.php';
 
@@ -7,14 +9,17 @@ require_once __DIR__ . '/../../data/db_config.php';
 $email = isset($_POST['email']) ? $_POST['email'] : '';
 $password = isset($_POST['password']) ? $_POST['password'] : '';
 
+
 // Create a User instance
 $user = new User($db);
 
-// Attempt to log in
+
+
 $result = $user->login($email, $password);
 
+
 if ($result) {
-    // Login successful
+    
     $_SESSION['user_id'] = $result['user_id'];
     $_SESSION['role'] = $result['role'];
     header('Location: dashboard.php');
