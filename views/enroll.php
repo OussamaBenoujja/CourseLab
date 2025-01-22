e0bba1b646537ef0514ef43360e95790eace96a9
OussamaBenoujja <osama2code79@gmail.com>
2025-01-22 07:26:56 +0100

    Add enroll functionality

---
+++ views/enroll.php
@@ -0,0 +1,23 @@
+
+
+<?php
+
+session_start();
+
+require_once '../control/User.php';
+require_once '../control/Student.php';
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit();
+}
+
+$st = new Student($db);
+$st->setUserId($_SESSION['user_id']);
+if(isset($_GET['course_id'])){
+    $course_id = $_GET['course_id'];
+    $st->joinCourse($course_id);
+    header('Location: home.php?enrolled=true');
+}
+
+?>
